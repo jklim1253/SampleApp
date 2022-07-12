bf864cf0a26831167ab6e976f292b70c8be92b1f
jklim1253 <jklim1253@gmail.com>
2022-07-13 03:21:37 +0900

    add migration, insert sample data button

--- SampleApp/ViewModels/SettingViewModel.cs
+++ SampleApp/ViewModels/SettingViewModel.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.Toolkit.Mvvm.Input;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.Input;
+using SampleModel;
+using SampleModel.Entity;
+using SampleModel.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,67 +13,54 @@ using System.Threading.Tasks;
 
 namespace SampleApp.ViewModels
 {
-  public class TestData
-  {
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-
-    public override bool Equals(object? obj)
-    {
-      return Id == (obj as TestData)?.Id;
-    }
-
-    public override int GetHashCode()
-    {
-      return Id;
-    }
-  }
-
   public class SettingViewModel : ViewModelBase
   {
-    private TestData selected = null!;
+    private readonly ILogger<SettingViewModel> logger;
+    private readonly SampleDataService dbService;
 
-    public TestData Selected
-    {
-      get => selected;
-      set => SetProperty(ref selected, value);
-    }
+    public IRelayCommand MigrationCommand { get; set; }
+    public IRelayCommand InsertSampleCommand { get; set; }
 
-    private IList<TestData> depot = null!;
-
-    public IList<TestData> Depot
-    {
-      get => depot;
-      set => SetProperty(ref depot, value);
-    }
-    
-    public IRelayCommand ClickCommand { get; set; }
-    public IRelayCommand Click2Command { get; set; }
-
-    public SettingViewModel()
+    public SettingViewModel(ILogger<SettingViewModel> logger, SampleDataService dbService)
     {
       Title = "ViewModel-Setting";
 
-      Depot = new List<TestData>()
+      this.logger = logger;
+      this.dbService = dbService;
+
+      MigrationCommand = new RelayCommand(OnMigration);
+      InsertSampleCommand = new RelayCommand(OnInsertSample);
+    }
+
+    private void OnMigration()
+    {
+      using (var context = Ioc.Default.GetRequiredService<SampleContext>())
       {
-        new TestData(){ Id = 1, Name = "hello" },
-        new TestData(){ Id = 2, Name = "world" },
-        new TestData(){ Id = 3, Name = "jklim" },
-      };
-
-      ClickCommand = new RelayCommand(OnClick);
-      Click2Command = new RelayCommand(OnClick2);
+        context.Database.Migrate();
+      }
     }
+    private async void OnInsertSample()
+    {
+      // view
+      await dbService.InsertViewInfo(new ViewInfo() { ViewName = "HomeView" });
+      await dbService.InsertViewInfo(new ViewInfo() { ViewName = "AccountView" });
+      await dbService.InsertViewInfo(new ViewInfo() { ViewName = "GroupView" });
+      await dbService.InsertViewInfo(new ViewInfo() { ViewName = "ViewView" });
+      await dbService.InsertViewInfo(new ViewInfo() { ViewName = "SettingView" });
 
-    private void OnClick()
-    {
-      Selected = Depot[2];
-    }
-    private void OnClick2()
-    {
-      Selected = new TestData() { Id = 1, Name = "hello" };
-      //var temp = Selected;
-      //Selected = Depot.FirstOrDefault(e => e.Id == Selected.Id);
+      // group
+      await dbService.InsertGroupInfo(new GroupInfo() { GroupName = "Administrators" });
+      await dbService.InsertGroupInfo(new GroupInfo() { GroupName = "Managers" });
+      await dbService.InsertGroupInfo(new GroupInfo() { GroupName = "Operators" });
+      await dbService.InsertGroupInfo(new GroupInfo() { GroupName = "Guests" });
+
+      // account
+      await dbService.InsertUserInfo(new UserInfo() { UserName = "admin", Password = "admin$123", GroupId = 1 });
+      await dbService.InsertUserInfo(new UserInfo() { UserName = "manager-1", Password = "manager$123" });
+      await dbService.InsertUserInfo(new UserInfo() { UserName = "operator-1", Password = "op$123" });
+      await dbService.InsertUserInfo(new UserInfo() { UserName = "operator-2", Password = "op$123" });
+      await dbService.InsertUserInfo(new UserInfo() { UserName = "operator-3", Password = "op$123" });
+      await dbService.InsertUserInfo(new UserInfo() { UserName = "guest", Password = "guest$123" });
     }
   }
 }
